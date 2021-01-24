e8a06773fb707542933205b6e0aeac05055b8ff2
Suzanne Cabral <suzannecabral@live.com>
2021-01-24 11:11:57 -0800

    Updated dbconfig environment

--- data/db-config.js
+++ data/db-config.js
@@ -6,5 +6,5 @@
 require("dotenv").config();
 const knex = require("knex")
 const config = require("../knexfile")
-const environment = process.env.NODE_ENV || "production";
+const environment = process.env.NODE_ENV || "development";
 module.exports = knex(config[environment])
